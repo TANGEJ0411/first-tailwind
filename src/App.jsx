1b2c91c01a6929eca64e4e6812f7c2f623a8c1e7
7238 唐翊傑 ejtang <11305001@pershing.com.tw>
2024-11-18 10:01:35 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="text-white bg-sky-700 hover:bg-sky-800">...</button>
+      <p className="text-red-200 hover:text-yellow-900">5555555</p>
+      <button className="btn">456</button>
+    </>
+  );
+}
+
+export default App;
